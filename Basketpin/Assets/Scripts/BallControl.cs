187d3feb7b6edf208e5107c77f587cd07244c58a
Kagan Ayten <kaan.aytn@hotmail.com>
2020-12-04 13:35:51 +0300

    Basketball

---
+++ Basketpin/Assets/Scripts/BallControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+    private bool isHooped;
+    private int score;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "HoopTrigger")
+        {
+            isHooped = true;
+        }
+        if (collision.gameObject.tag == "ScoreTrigger")
+        {
+            score += 1;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "HoopTrigger")
+        {
+            isHooped = false;
+        }
+    }
+    public bool GetIsHooped()
+    {
+        return this.isHooped;
+    }
+    public int GetScore()
+    {
+        return this.score;
+    }
+}
--- Basketpin/Assets/Scripts/GameSystem.cs
+++ Basketpin/Assets/Scripts/GameSystem.cs
@@ -1,37 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameSystem : MonoBehaviour
 {
     public GameObject rightArm;
     public GameObject leftArm;
+    public GameObject ball;
+    public GameObject hoop;
+    public Text scoreText;
 
-    public float throwForce;
+    [SerializeField]
+    [Range(500,2500)]
+    private float throwForce;
+
+    private int score;
 
     private Rigidbody2D rbRight;
     private Rigidbody2D rbLeft;
+
+    private Collider2D hoopColl;
+    private BallControl ballData;
     void Start()
     {
         rbRight = rightArm.GetComponent<Rigidbody2D>();
         rbLeft = leftArm.GetComponent<Rigidbody2D>();
+        hoopColl = hoop.GetComponent<BoxCollider2D>();
+        ballData = ball.GetComponent<BallControl>();
     }
 
     void Update()
     {
+        scoreText.text = ballData.GetScore().ToString();
         if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 inputPos = Input.mousePosition;
+            if (inputPos.x >= Screen.width / 2f)
             {
-                Vector3 inputPos = Input.mousePosition;
-                if (inputPos.x >= Screen.width / 2f)
-                {
                 Throw(rbRight, -throwForce);
-                }
-
-                if (inputPos.x < Screen.width / 2f)
-                {
-                Throw(rbLeft, throwForce);
-                }
             }
+            if (inputPos.x < Screen.width / 2f)
+            {
+                Throw(rbLeft, throwForce);
+            }
+        }
+        
+        if(ballData.GetIsHooped())
+        {
+            hoopColl.enabled = false;
+        }
+        else
+        {
+            hoopColl.enabled = true;
+        }
     }
     void Throw(Rigidbody2D rb,float force)
     {
